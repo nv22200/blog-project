ccfbc348da1d84fcc10d84fab625530c332cb893
nv22200 <154304439+nv22200@users.noreply.github.com>
2024-03-06 18:02:00 +0000

    9 - Article detail page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import HomePage from './pages/home/HomePage';
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
     <div className="App font-opensans">
-      <HomePage />
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route index path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
     </div>
   );
 }
