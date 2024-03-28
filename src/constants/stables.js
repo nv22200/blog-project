f12383414841d00f8eb22398a702a4dead2c5f3b
nv22200 <154304439+nv22200@users.noreply.github.com>
2024-03-28 17:13:09 +0000

    25 - Profile page

---
+++ src/constants/stables.js
@@ -0,0 +1,5 @@
+const UPLOAD_FOLDER_BASE_URL = "https://verbose-space-bassoon-r4699qv5wjvh6rj-3000.app.github.dev/uploads/";
+
+const stables = { UPLOAD_FOLDER_BASE_URL };
+
+export default stables;
