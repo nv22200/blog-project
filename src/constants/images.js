fb998ab918f9ea677e4a5e82eed68442e5edd076
nv22200 <154304439+nv22200@users.noreply.github.com>
2024-05-04 17:39:21 +0000

    34 - show all posts in home page

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,6 +3,8 @@ import HeroImage from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
 import CtaImage from "../assets/CtaImage.jpg";
+const samplePostImage = "/images/sample.jpg";
+const userImage = "/images/user.png";
 
 const images = {
   Logo,
@@ -10,6 +12,8 @@ const images = {
   Post1Image,
   PostProfileImage,
   CtaImage,
+  samplePostImage,
+  userImage,
 };
 
 export default images;
